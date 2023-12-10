e90de929d540e3ec758437be56217ccc54cba3d9
Sahil Sharma <sahilbhardwaj549@gmail.com>
2023-12-10 13:07:36 +0530

    Add - Profile Card ui

--- app/src/main/java/com/sahil/myapplication/MainActivity.kt
+++ app/src/main/java/com/sahil/myapplication/MainActivity.kt
@@ -1,27 +1,34 @@
-package com.sahil.jetpackcomposeprectice
+package com.sahil.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -29,50 +36,65 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    val emailState = remember { mutableStateOf<String>("") }
-    val password = remember { mutableStateOf<String>("") }
+fun MainScreen(){
+    Surface(modifier = Modifier.fillMaxSize(),
+        color= Color.LightGray
+    ) {
+        ProfileCard()
+    }
+}
 
-    Surface(modifier = Modifier.fillMaxSize(), color = Color.LightGray) {
-        Column(Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
 
-            TextField(
-                value = emailState.value,
-                onValueChange = { text -> emailState.value = text },
-                placeholder = { Text("Enter email") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
-            )
-            TextField(
-                value = password.value,
-                onValueChange = { text -> password.value = text },
-                placeholder = { Text("Enter password") },
-                singleLine = true,
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp)
-            )
-            Button(
-                onClick = { },
-                modifier = Modifier.padding(top = 20.dp)
-            ) {
-                Text(text = "Submit")
-            }
+@Composable
+fun ProfileCard(){
+    Card(modifier= Modifier
+        .fillMaxWidth()
+        .wrapContentHeight(align = Alignment.Top)
+        .padding(16.dp),
+        elevation = CardDefaults.cardElevation(8.dp)) {
+        Row (modifier = Modifier.wrapContentSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start){
+            ProfilePicture()
+            ProfileContent()
         }
     }
 }
 
+@Composable
+fun ProfileContent() {
+    Column(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()) {
+        Text(text = "Jhon Deo", style = MaterialTheme.typography.headlineSmall)
+        Text(text = "Active now",
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier.alpha(0.5f))
+    }
+
+}
+
+@Composable
+fun ProfilePicture() {
+    Card(shape = CircleShape,
+        border = BorderStroke(2.dp, Color.Red),
+        modifier = Modifier.padding(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Image(painter = painterResource(id = R.drawable.profile_1), contentDescription ="Profile image" ,Modifier.size(70.dp))
+
+    }
+
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Greeting("Android")
+    MainScreen()
 }
